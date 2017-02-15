6fe1f0bfa067754911db050ebd647a4588fe0844
vinisdl <vinisdl@hotmail.com>
2017-02-15 17:33:49 -0200

    add dinamic connection strings

--- Totosinho.Infra.Contexto/Context/TotosinhoContext.cs
+++ Totosinho.Infra.Contexto/Context/TotosinhoContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Configuration;
+using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using Totosinho.Domain.Entidades;
 using Totosinho.Repositorio.Context.Seed;
@@ -9,11 +10,35 @@ namespace Totosinho.Repositorio.Context
     public class TotosinhoContext : DbContext
     {
         public TotosinhoContext()
-            : base("DefaultConnectionDev")
+            : base(GetSqlConnection())
         {
             Database.SetInitializer(new TotosinhoDbInitializer());
             Configuration.LazyLoadingEnabled = false;
         }
+
+        public static string GetSqlConnection()
+        {
+            string _connectionString;
+            var ambiente = ConfigurationManager.AppSettings["ambiente"].ToLower();
+            switch (ambiente)
+            {
+                case "producao":
+                    _connectionString = ConfigurationManager.ConnectionStrings["DefaultConnectionProducao"].ConnectionString;
+                    break;
+                case "teste":
+                    _connectionString = ConfigurationManager.ConnectionStrings["DefaultConnectionTeste"].ConnectionString;
+                    break;
+                case "dev":
+                    _connectionString = ConfigurationManager.ConnectionStrings["DefaultConnectionDev"].ConnectionString;
+                    break;
+                default:
+                    _connectionString = ConfigurationManager.ConnectionStrings["DefaultConnectionDev"].ConnectionString;
+                    break;
+            }
+            return _connectionString;
+        }
+
+
         public DbSet<Servidor> Servidor { get; set; }
         public DbSet<Score> Score { get; set; }
 
